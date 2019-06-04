7e7ccc2e992c2c617f0d32fbfa56e1c0de49146e
lili14gon <liligh14@gmail.com>
2019-06-04 12:25:15 -0500

    1era imagen

--- README.md
+++ README.md
@@ -20,6 +20,8 @@
   ser cifrado o descifrado con una respectiva clave, si el texto fue cifrado se tendra que copiar dicho texto 
   cifrado para ser pegado en nuestra bandeja de salida del correo electronico, tambien se enviara al receptor
   la clave para que pueda descifrarlo en la aplicacion web. 
+  
+  !["Imagen del 1er Prototipo"](imag/im1.jpg)
 
   El feeback recibido por mis compañeras fue que al momento de obtener el texto cifrado hacerle recordar al usuario
   la clave que ha utilizado para encriptar el texto y asi poder tener presente al momento enviarlo.
--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-/* Acá va tu código */
+const pasword=document.getElementById("password");
+const ingresar=document.getElementById("btnIngresar");
+const mensaje=document.getElementById("titulo");
+const secLogin=document.getElementById("secLogin");
+const secCodyDec=document.getElementById("secCodyDec");
+
+ingresar.addEventListener('click',login);
+let contador=0;
+secCodyDec.style.display="none";
+
+function login(){
+	if(pasword.value==='LABORATORIA'){
+    mensaje.innerHTML;
+    secLogin.style.display="none";
+    secCodyDec.style.display="flex";
+
+            //  alert("correcto");
+	}
+	if(password.value!=='LABORATORIA') {
+		contador=contador+1;
+	    if (contador<=2){
+	    	alert("incorrecto");
+	    }
+	    else{
+	    	alert("Ya utilizaste todos tus intentos, en este momento no podrás ingresar");
+	    }
+		
+	}
+}
+
