55bd4cc14119f2c9baf103ddd8a0c5bb5cef0013
lili14gon <liligh14@gmail.com>
2019-06-12 18:28:09 -0500

    modificando estilos

--- src/cipher.js
+++ src/cipher.js
@@ -5,8 +5,12 @@ window.cipher = {
     for (let i = 0; i < string.length; i++) {
       if (string.charCodeAt(i) === 32) {
         NumAscci = string.charCodeAt(i);
-      } else {
+      } 
+      else if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
         NumAscci = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+      }
+      else if(string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122){
+        NumAscci = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
       }
       Nuevaletra = Nuevaletra + String.fromCharCode(parseInt(NumAscci));
     }
@@ -18,8 +22,12 @@ window.cipher = {
     for (let i = 0; i < string.length; i++) {
       if (string.charCodeAt(i) == 32) {
         NumAscci1 = string.charCodeAt(i);
-      } else {
+      } 
+      else if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90){
         NumAscci1 = 90 - ((90 - string.charCodeAt(i) + parseInt(offset)) % 26);
+      }
+      else if(string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122){
+        NumAscci1 = 122 - ((122 - string.charCodeAt(i)+ parseInt(offset)) % 26);
       }
       Nuevaletra1 = Nuevaletra1 + String.fromCharCode(parseInt(NumAscci1));
     }
