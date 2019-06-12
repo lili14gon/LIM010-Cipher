78abcbbfd7b0bf2760e607a473b78bbcb3aa6a70
lili14gon <liligh14@gmail.com>
2019-06-12 01:30:16 -0500

    modificando estilos

--- src/cipher.js
+++ src/cipher.js
@@ -1,44 +1,28 @@
-
-
-// let NumAscci=0;
-// let Nuevaletra='';
-
 window.cipher = {
-
   encode: (offset, string) => {
-      let NumAscci=0;
-      let Nuevaletra='';
-
-     for(let i=0;i<string.length;i++){
-      if(string.charCodeAt(i)===32){
-        NumAscci=string.charCodeAt(i);
-        }
-      else{
-        NumAscci=(string.charCodeAt(i)-65+parseInt(offset))%26+65;
-        }
-
-        Nuevaletra=Nuevaletra+String.fromCharCode(parseInt(NumAscci));
-     }
-     //respuesta.innerHTML=Nuevaletra;
-     return Nuevaletra;
-    /* Acá va tu código que cifra*/
+    let NumAscci = 0;
+    let Nuevaletra = '';
+    for (let i = 0; i < string.length; i++) {
+      if (string.charCodeAt(i) === 32) {
+        NumAscci = string.charCodeAt(i);
+      } else {
+        NumAscci = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+      }
+      Nuevaletra = Nuevaletra + String.fromCharCode(parseInt(NumAscci));
+    }
+    return Nuevaletra;
   },
   decode: (offset, string) => {
-  	let NumAscci1=0;
-  	let Nuevaletra1='';
-  	for(let i=0; i<string.length;i++){
-  		if(string.charCodeAt(i)==32){
-         NumAscci1=string.charCodeAt(i);
-  		    }
-        else{
-
-        	//NumAscci1=(string.charCodeAt(i)-65-parseInto(offset)+26*2)%26+65;
-        NumAscci1=90-((90-string.charCodeAt(i)+parseInt(offset))%26);
-        }  
-        Nuevaletra1=Nuevaletra1+String.fromCharCode(parseInt(NumAscci1));
-  	   }
-  	   return Nuevaletra1;
-    /* Acá va tu código que descifra*/
-  
-   }
+    let NumAscci1 = 0;
+    let Nuevaletra1 = '';
+    for (let i = 0; i < string.length; i++) {
+      if (string.charCodeAt(i) == 32) {
+        NumAscci1 = string.charCodeAt(i);
+      } else {
+        NumAscci1 = 90 - ((90 - string.charCodeAt(i) + parseInt(offset)) % 26);
+      }
+      Nuevaletra1 = Nuevaletra1 + String.fromCharCode(parseInt(NumAscci1));
+    }
+    return Nuevaletra1;
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,92 +1,70 @@
-const pasword=document.getElementById("password");
-// const respuesta=document.getElementById("idResultado");
-
+const password=document.getElementById("password");
+//secciones
 const secLogin=document.getElementById("secLogin");
 const secCodyDec=document.getElementById("secCodyDec");
 const secResultado=document.getElementById("secResultado");
-const secBloqueo=document.getElementById("secBloqueo");
-
+//botones
 const ingresar=document.getElementById("btnIngresar");
 const codificar=document.getElementById("btnCodificar");
 const decodificar=document.getElementById("btnDecodificar");
 const volver=document.getElementById("btnVolver");
 const finalizar=document.getElementById("btnFinalizar");
-
+//Funcion decodificar
 codificar.addEventListener('click',()=>{
-
-	 // secLogin.style.display="none";
+  // secLogin.style.display="none";
   //    secCodyDec.style.display="none";
   //    secResultado.style.display="block";  
-
   secLogin.classList.add('hide');
   secCodyDec.classList.add('hide');
   secResultado.classList.remove('hide');
-
     const llave=document.getElementById("number").value;
     const texto=document.getElementById("menCodyDec").value;
-
     const respuesta=document.getElementById("idResultado");
     const recLlave=document.getElementById("recLlave")
-
     let Resultado=cipher.encode(llave,texto);
-
     recLlave.innerHTML=llave;
     respuesta.innerHTML=(Resultado);
-
 });
-
+//Funcion decodificar
 decodificar.addEventListener('click',()=>{
     secLogin.classList.add('hide');
     secCodyDec.classList.add('hide');
     secResultado.classList.remove('hide');
-
-
     const llave=document.getElementById("number");
     const texto=document.getElementById("menCodyDec");
-
     const respuesta=document.getElementById("idResultado");
     const recLlave=document.getElementById("recLlave")
-
     let Resultado=cipher.decode(llave.value,texto.value);
     recLlave.innerHTML=llave.value;
     respuesta.innerHTML=(Resultado);
-
 });
-
-
-
 ingresar.addEventListener('click',Login);
 volver.addEventListener('click',Volver);
 finalizar.addEventListener('click',Finalizar);
-
 let contador=0;
+//Funcion login
 function Login(){
-  
-	if(pasword.value==='LABORATORIA'){
-    
+	if(password.value==='LABORATORIA'){
     secLogin.classList.add('hide');
     secCodyDec.classList.remove('hide');
 	}
-
 	if(password.value!=='LABORATORIA') {
-		if(pasword.value===''){
+		if(password.value===''){
 			alert("ingresar pasword");
 		}
-		else{
+		else
+        {
 		contador=contador+1;
-	           if (contador<=2){
-	    	       alert("incorrecto");
-	    	
-	             }
-	           else{
-                    //secLogin.classList.add('hide');
-                    // secBloqueo.classList.remove('hide');
-                    pasword.disabled = true;
-                    alert("Ya utilizaste todos tus intentos, en este momento no podrás ingresar");
-
-	    	       }
-	    }
- 
+            if (contador<=2){
+                alert("incorrecto");
+            }
+            else{
+           //secLogin.classList.add('hide');
+            alert("Ya utilizaste todos tus intentos, en este momento no podrás ingresar");
+            password.disabled = true;
+            ingresar.disabled = true;
+            }
+        }
     }
  document.getElementById("password").value="";
 }
@@ -94,21 +72,14 @@ function Volver(){
  secLogin.classList.add('hide');
  secResultado.classList.add('hide');
  secCodyDec.classList.remove('hide'); 
-
  document.getElementById("menCodyDec").value="";
  document.getElementById("number").value="";
  }
-
  function Finalizar(){
  secLogin.classList.remove('hide');
  secCodyDec.classList.add('hide');
  secResultado.classList.add('hide');
-
  document.getElementById("password").value="";
  document.getElementById("menCodyDec").value="";
  document.getElementById("number").value="";
-
  }
-
-
-
